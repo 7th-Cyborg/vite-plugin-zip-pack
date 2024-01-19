a01f84c57691dcb90b4fa17665b769d0e0bb332f
reinerBa <4329883+reinerBa@users.noreply.github.com>
2024-01-19 23:06:19 +0100

    Add done-callback function

--- src/index.ts
+++ src/index.ts
@@ -28,6 +28,11 @@ export interface Options {
    * @default ``
    */
   pathPrefix?: string;
+  /**
+   * Callback, which is executed after the zip file was created
+   * err is only defined if the save function fails
+   */
+  done?: (err: Error | undefined) => void
 }
 
 export default function zipPack(options?: Options): PluginOption {
@@ -35,35 +40,42 @@ export default function zipPack(options?: Options): PluginOption {
   const outDir = options?.outDir || "dist-zip";
   const outFileName = options?.outFileName || "dist.zip";
   const pathPrefix = options?.pathPrefix || '';
+  const done = options?.done || function (){};
 
-  function addFilesToZipArchive(zip: JSZip | null, inDir: string) {
-    const listOfFiles = fs.readdirSync(inDir);
+  async function addFilesToZipArchive(zip: JSZip, inDir: string) {
+    const listOfFiles = await fs.promises.readdir(inDir);
 
-    listOfFiles.forEach((fileName) => {
+    for (const fileName of listOfFiles) {
       const filePath = path.join(inDir, fileName);
-      const file = fs.statSync(filePath);
+      const file = await fs.promises.stat(filePath);
       const timeZoneOffsetDate = timeZoneOffset(new Date(file.mtime));
 
-      if (file?.isDirectory()) {
-        zip!.file(fileName, null, {
+      if (file.isDirectory()) {
+        zip.file(fileName, null, {
           dir: true,
           date: timeZoneOffsetDate
         });
-        const dir = zip!.folder(fileName);
+        const dir = zip.folder(fileName);
+        if(!dir) {
+          throw new Error(`fileName '${fileName}' couldn't get included als directory in the zip`);
+        }
 
-        addFilesToZipArchive(dir, filePath);
+        await addFilesToZipArchive(dir, filePath);
       } else {
-
-        zip!.file(fileName, fs.readFileSync(filePath), { date: timeZoneOffsetDate });
+        zip.file( 
+          fileName, 
+          await fs.promises.readFile(filePath), 
+          { date: timeZoneOffsetDate }
+        );
       }
-    });
+    }
   }
 
-  function createZipArchive(zip: JSZip | null) {
+  async function createZipArchive(zip: JSZip) {
     // @ts-ignore
     zip.root = '';
 
-    zip!
+    const file = await zip
       .generateAsync({
         type: "nodebuffer",
         compression: "DEFLATE",
@@ -71,62 +83,69 @@ export default function zipPack(options?: Options): PluginOption {
           level: 9,
         },
       })
-      .then((file) => {
-        const fileName = path.join(outDir, outFileName);
+      
+    const fileName = path.join(outDir, outFileName);
 
-        if (fs.existsSync(fileName)) {
-          fs.unlinkSync(fileName);
-        }
+    if (fs.existsSync(fileName)) {
+      await fs.promises.unlink(fileName);
+    }
 
-        fs.writeFileSync(fileName, file);
-      });
+    await fs.promises.writeFile(fileName, file)
+    done(undefined)
   }
 
   return {
     name: "vite-plugin-zip-pack",
     apply: "build",
     enforce: "post",
-    closeBundle() {
+    async closeBundle() {
       try {
         console.log("\x1b[36m%s\x1b[0m", `Zip packing - "${inDir}" folder :`);
-        if (fs.existsSync(inDir)) {
-          if (!fs.existsSync(outDir)) {
-            fs.mkdirSync(outDir);
-          }
-
-          if (pathPrefix && path.isAbsolute(pathPrefix)) {
-            throw new Error('"pathPrefix" must be a relative path');
-          }
-
-          const zip = new JSZip();
-          let archive;
-
-          if (pathPrefix) {
-            const timeZoneOffsetDate = timeZoneOffset(new Date());
-
-            zip!.file(pathPrefix, null, {
-              dir: true,
-              date: timeZoneOffsetDate
-            });
-            archive = zip!.folder(pathPrefix);
-          } else {
-            archive = zip;
-          }
-
-          console.log("\x1b[32m%s\x1b[0m", "  - Preparing files.");
-          addFilesToZipArchive(archive, inDir);
-
-          console.log("\x1b[32m%s\x1b[0m", "  - Creating zip archive.");
-          createZipArchive(archive);
-
-          console.log("\x1b[32m%s\x1b[0m", "  - Done.");
-        } else {
-          console.log(
+        
+        if (!fs.existsSync(inDir)) {
+          return console.log(
             "\x1b[31m%s\x1b[0m",
             `  - "${inDir}" folder does not exist!`
           );
         }
-      } catch (error) {
+
+        if (!fs.existsSync(outDir)) {
+          await fs.promises.mkdir(outDir)
+        }
+
+        if (pathPrefix && path.isAbsolute(pathPrefix)) {
+          throw new Error('"pathPrefix" must be a relative path');
+        }
+
+        const zip = new JSZip();
+        let archive: JSZip;
+
+        if (pathPrefix) {
+          const timeZoneOffsetDate = timeZoneOffset(new Date());
+
+          zip.file(pathPrefix, null, {
+            dir: true,
+            date: timeZoneOffsetDate
+          });
+          const zipFolder = zip.folder(pathPrefix);
+          
+          if(zipFolder) {
+            archive = zipFolder;
+          } else {
+            throw new Error("Files could not be loaded from 'pathPrefix'")
+          }
+        } else {
+          archive = zip;
+        }
+
+        console.log("\x1b[32m%s\x1b[0m", "  - Preparing files.");
+        addFilesToZipArchive(archive, inDir);
+
+        console.log("\x1b[32m%s\x1b[0m", "  - Creating zip archive.");
+        await createZipArchive(archive)
+
+        console.log("\x1b[32m%s\x1b[0m", "  - Done.");
+      } catch (error: any) {
         if (error) {
           console.log(
             "\x1b[31m%s\x1b[0m",
@@ -138,7 +157,8 @@ export default function zipPack(options?: Options): PluginOption {
           "\x1b[31m%s\x1b[0m",
           "  - Something went wrong while building zip file!"
         );
+        done(error)
       }
-    },
+    }
   };
 }
